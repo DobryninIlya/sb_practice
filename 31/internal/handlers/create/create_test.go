88ee83370b84dcb4f1e21cbd44caceec7cf84711
Ilya <mr.woodysimpson@gmail.com>
2023-03-15 15:00:50 +0300

    added sample for test case

--- 31/internal/handlers/create/create_test.go
+++ 31/internal/handlers/create/create_test.go
@@ -2,6 +2,7 @@ package create
 
 import (
 	"main.go/internal/handlers/create/mocks"
+	"main.go/internal/user"
 	"net/http"
 	"reflect"
 	"testing"
@@ -10,6 +11,7 @@ import (
 func TestNew(t *testing.T) {
 	type args struct {
 		creator UserCreator
+		user    user.User
 	}
 	tests := []struct {
 		name string
@@ -18,11 +20,19 @@ func TestNew(t *testing.T) {
 	}{
 		{
 			name: "base test",
+			args: args{
+				user: user.User{
+					Name:   "lalala",
+					Age:    66,
+					Friend: []int{},
+				},
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			userCreator := mocks.NewUserCreator(t)
+			userCreator.On("CreateUser", tt.args.user).Return(20)
 			if got := New(tt.args.creator); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("New() = %v, want %v", got, tt.want)
 			}
