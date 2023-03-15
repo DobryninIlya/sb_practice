c42486c0de6087a2be11bcb6d842c8af1570f10c
Ilya <mr.woodysimpson@gmail.com>
2023-03-15 15:42:19 +0300

    edit last sample

--- 31/internal/handlers/create/create_test.go
+++ 31/internal/handlers/create/create_test.go
@@ -1,12 +1,23 @@
 package create
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
 	"main.go/internal/handlers/create/mocks"
 	"main.go/internal/user"
 	"net/http"
-	"reflect"
+	"net/http/httptest"
 	"testing"
 )
+
+type userParams struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Friends []int  `json:"friends"`
+}
 
 func TestNew(t *testing.T) {
 	type args struct {
@@ -29,13 +40,29 @@ func TestNew(t *testing.T) {
 			},
 		},
 	}
+	bytesRepresentation, err := json.Marshal(userParams{"test", 2, []int{}})
+	if err != nil {
+		log.Fatalln(err)
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			userCreator := mocks.NewUserCreator(t)
 			userCreator.On("CreateUser", tt.args.user).Return(20)
-			if got := New(tt.args.creator); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
+			got := New(tt.args.creator)
+			srv := httptest.NewServer(http.HandlerFunc(got))
+
+			resp, err := http.Post(srv.URL, "text/json", bytes.NewBuffer(bytesRepresentation))
+			if err != nil {
+				t.Log(err)
+				t.Fail()
 			}
+			defer resp.Body.Close()
+			textBytes, err := io.ReadAll(resp.Body)
+			fmt.Println(textBytes)
+
+			//if got := New(tt.args.creator); !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("New() = %v, want %v", got, tt.want)
+			//}
 		})
 	}
 }
